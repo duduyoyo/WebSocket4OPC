52516e11060b213d582827d2e5eed6cf7775e574
Jiyang Hu <jiyang_hu@yahoo.com>
2022-08-25 17:21:04 -0500

    Show meaningful parsed JSON in iOS APP

--- Client/Swift/Controller/WebSocketController.swift
+++ Client/Swift/Controller/WebSocketController.swift
@@ -1,6 +1,33 @@
 import Foundation
 import SwiftUI
 
+struct DA: Codable {
+    let da: [Da]?
+
+    enum CodingKeys: String, CodingKey {
+        case da = "DA"
+    }
+}
+
+struct Da: Codable {
+    let i, v: String
+    let t, q: Int
+}
+
+struct OpcServers: Codable {
+    let da, ae: Int?
+
+    enum CodingKeys: String, CodingKey {
+        case da = "DA"
+        case ae = "AE"
+    }
+}
+
+struct Browse: Codable {
+    let n, i: String
+    let b: Int
+}
+
 class myDelegate: NSObject, URLSessionWebSocketDelegate {
 }
 
@@ -50,11 +77,32 @@ class WebSocketController : ObservableObject {
             case .success(let message):
                 switch message {
                 case .string(let str):
+                    let jsonData = str.data(using: .utf8)!
+                    if str.starts(with: "[{\"D"){
+                        let servers = try! JSONDecoder().decode([OpcServers].self, from: jsonData)
+                        let da = servers[0].da
+                        let ae = servers[1].ae
+
+                        DispatchQueue.main.async {
+                            self.alertWrapper.append(AlertWrapper(alert:DA(da: nil), servers: OpcServers(da: da, ae: ae), browseResult: nil))
+                        }
+                    } else if str.starts(with: "[{\"n"){
+                        let browse = try! JSONDecoder().decode([Browse].self, from: jsonData)
+
+                        DispatchQueue.main.async {
+                            self.alertWrapper.append(AlertWrapper(alert:DA(da: nil), servers: OpcServers(da: nil, ae: nil), browseResult: browse))
+                        }
+                    } else if str.starts(with: "{\"D"){
+                        let da = try! JSONDecoder().decode(DA.self, from: jsonData)
+
+                        DispatchQueue.main.async {
+                            self.alertWrapper.append(AlertWrapper(alert:da, servers: OpcServers(da: nil, ae: nil), browseResult: nil))
+                        }
+                    }
+                    
                     print(str)
                     
-                    DispatchQueue.main.async {
-                        self.alertWrapper.append(AlertWrapper(alert:str))
-                    }
+                   break
                 case .data(_):
                     break
                 @unknown default:
--- Client/Swift/Model and View/ContentView.swift
+++ Client/Swift/Model and View/ContentView.swift
@@ -2,7 +2,9 @@ import SwiftUI
 
 struct AlertWrapper: Identifiable {
     let id = UUID()
-    var alert: String
+    var alert: DA
+    var servers:OpcServers
+    var browseResult:[Browse]?
 }
 
 class ViewModel{
@@ -14,27 +16,73 @@ struct ContentView: View {
     @State var buttonLabel = "Disconnect"
     var myModel : ViewModel
     
-    @ObservedObject var controoler:WebSocketController
+    @ObservedObject var controller:WebSocketController
     
     init(){
         myModel = ViewModel()
-        controoler = WebSocketController(alertWrapper:myModel.alertWrapper)
+        controller = WebSocketController(alertWrapper:myModel.alertWrapper)
+    }
+    
+    func alertMapped(myAlertWrapper: AlertWrapper) -> String {
+       
+        if myAlertWrapper.alert.da != nil {
+            
+            let timestamp = Double(myAlertWrapper.alert.da![0].t)
+            let date = Date(timeIntervalSince1970: timestamp)
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "yyyy/MM/dd HH:mm:ss"
+            let time = dateFormatter.string(from: date)
+            
+            return myAlertWrapper.alert.da![0].i+": "+myAlertWrapper.alert.da![0].v+"@"+time
+            
+        } else if myAlertWrapper.servers.da != nil && myAlertWrapper.servers.ae != nil {
+            var result = ""
+            
+            if myAlertWrapper.servers.da == 1 {
+                result = "DA : Available"
+            }else{
+                result = "DA : N/A"
+            }
+            
+            if myAlertWrapper.servers.ae == 1 {
+                result = result + "\t\t\t" + "AE : Available"
+            }else{
+                result = result + "\t\t\t\t" + "AE : N/A"
+            }
+            
+            return result
+            
+        } else if myAlertWrapper.browseResult != nil {
+            var result = "Top\n\t"
+            myAlertWrapper.browseResult!.forEach { browse in
+                
+                if browse.b == 1{
+                    result = result+browse.n+" +\n\t"
+                }else{
+                    result = result+browse.n+" -\n\t"
+                }
+            }
+            return result
+        }
+        
+        return "n/a"
     }
     
     var body: some View {
         
         VStack(spacing: 1) {
-            List(controoler.alertWrapper){ myAlertWrapper in
-                Text(myAlertWrapper.alert)
+            List(controller.alertWrapper){ myAlertWrapper in
+                let result = alertMapped(myAlertWrapper:myAlertWrapper)
+                Text(result)
             }
             
             Divider()
             
             Button(buttonLabel, action: {
                 if self.buttonLabel == "Connect" {
-                    self.controoler.connect()
+                    self.controller.connect()
                 } else {
-                    self.controoler.disconnect()
+                    self.controller.disconnect()
                 }
                 self.buttonLabel = self.buttonLabel == "Connect" ? "Disconnect" : "Connect"
                 
